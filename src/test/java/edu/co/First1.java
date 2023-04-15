69bc7f29014f3af933940a4bb5872081e483a409
edureka <abhardwaj273@gmail.com>
2023-04-16 01:43:43 +0530

    first commit

--- src/test/java/edu/co/First1.java
+++ src/test/java/edu/co/First1.java
@@ -3,14 +3,22 @@ package edu.co;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class First1 {
+	
+	
   @Test
   public void f() {
-	  System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver_win32\\chromedriver.exe");  
+	  System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver.exe");  
+	  //System.setProperty("webdriver.http.factory", "jdk-http-client");
 		WebDriver driver = new ChromeDriver();  
+		//ChromeOptions chromeOptions = new ChromeOptions();
+		 //chromeOptions.addArguments("--remote-allow-origins=*");
+		 //ChromeDriver driver = new ChromeDriver(chromeOptions); 
 	      
 	// Launch website  
 	    driver.navigate().to("http://www.edureka.co/");  
